6714124d207ce15602fe8a67f319f56638d53a18
hedgemonkie <20231306@sungshin.ac.kr>
2025-08-13 03:59:52 +0900

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,9 +1,4 @@
 plugins {
-    // Android Gradle Plugin은 flutter-gradle-plugin이 관리하므로 여기서 선언하지 않음
-    // id("com.android.application") version "8.5.2" apply false  // ❌ 삭제
-    // kotlin("android") version "1.9.x" apply false               // ❌ 삭제
-
-    // Google Services만 버전 명시해서 제공
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
